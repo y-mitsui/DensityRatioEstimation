5a52fce3286ba9b69b0f73947bdef395e0237480
三井 陽介 <mitsuiyosuke@LAB-N1110.local>
2017-05-29 14:28:31 +0900

    implement uLSIF

---
+++ README.md
@@ -0,0 +1,21 @@
+Density Ratio Estimation
+=============
+
+Python liblary of the Density Ratio Estimation (Sugiyama).
+
+## Features
+* Direct estimate two ratio of probability distribution as high accuracy as follows.
+ 
+```
+r(sample) = P_x(sample) / P_y(sample)
+```
+
+* License: New BSD
+
+
+## Usage
+Read lsif.py.
+
+```
+python lsif.py
+```
--- lsif.py
+++ lsif.py
@@ -1,22 +1,23 @@
 # -*- coding:utf-8 -*-
 # uLSIF
-from scipy.stats import binom
-import pandas as pd
 import numpy as np
-import sys
-from scipy.stats import norm
-from numpy.random import *
-import math
-import matplotlib.pyplot as plt
 
 class LSIF:
     """
+        Density radio estimation of  uLSIF (Sugiyama) using RBF kernel
         r(sample) = P_x(sample) / P_y(sample)
+        
+        example:
+            LSIF(0.3, 0.1).fit(sample_molecule, sample_denominator).predict(sample_new)
     """
-    def __init__(self, band_width, regulation, n_kernel_fold=20):
+    
+    def __init__(self, band_width, regulation):
+        """
+        @param band_width: parameter of RBF kernel
+        @param regulation: regulation to prevent over fitting
+        """
         self.band_width = band_width
         self.regulation = regulation
-        self.n_kernel_fold = n_kernel_fold
     
     def kernel(self, X):
         result = []
@@ -24,10 +25,6 @@ class LSIF:
             diff_vec = X - X2
             result.append(np.exp(-np.dot(diff_vec, diff_vec) / (2*self.band_width**2)))
         return np.array(result)
-        
-    def kernel2(self, x, x2, band_width):
-        h = band_width
-        return np.exp(-(x-x2)**2/(2*h**2))
         
     def estGHat(self, sample_hospital):
         g_hat = np.matrix(np.zeros((self.n_kernel_fold, self.n_kernel_fold)),dtype=float)
@@ -54,11 +51,13 @@ class LSIF:
         return self._score(self.thetas, g_hat, h_hat) 
         
     def fit(self, sample_X, sample_Y):
+        self.n_kernel_fold = sample_X.shape[0]
         self.sample_kernel_fold = sample_X[:self.n_kernel_fold]
         g_hat = self.estGHat(sample_Y)
         h_hat = self.estHHat(sample_X)
         self.thetas = np.linalg.inv(g_hat + self.regulation * np.matrix(np.identity(self.n_kernel_fold))) * h_hat.T
         self.thetas = np.maximum(self.thetas, 0)
+        return self
         
     def predict(self, sample):
         result = []
@@ -68,39 +67,5 @@ class LSIF:
         return np.array(result)
 
     
-if __name__ == "__main__":
 
-    np.random.seed(123)
-
-    n_sample = 500
-    scale=3.
-
-    sample_X = randn(n_sample, 1) * scale + 1.5
-    sample_Y = randn(n_sample, 1) * scale + 1
-    
-    n_test = int(sample_X.shape[0] * 0.3)
-    sample_X_train = sample_X[:-n_test]
-    sample_X_test = sample_X[n_test:]
-    num_theta = sample_X.shape[0]
-    sample_Y_train = sample_Y[:-n_test]
-    sample_Y_test = sample_Y[n_test:]
-    
-    min_error = float('inf')
-    for band_width in np.linspace(2.5, 5, 0):
-        for regration in  np.linspace(5e-2, 2, 10):
-            lsif = LSIF(band_width, regration, sample_X_train.shape[0])
-            lsif.fit(sample_X_train, sample_Y_train)
-            score = lsif.score(sample_X_test, sample_Y_test)
-            print band_width, regration, score
-            if min_error > score:
-                min_error = score
-                min_params = (band_width, regration)
-                print "current best ", band_width, regration, min_error
-            
-    lsif = LSIF(3.61111111111, 0.48, sample_X.shape[0])
-    lsif.fit(sample_X, sample_Y)
-    prob_true = norm.pdf(sample_X, loc=1.5, scale=scale) / norm.pdf(sample_X, loc=1., scale=scale)
-    prob_est = lsif.predict(sample_X)
-    plt.scatter(prob_est, prob_true)
-    plt.show()
     
